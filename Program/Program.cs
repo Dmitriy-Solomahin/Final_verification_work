ec83a7c513e12407b6f0d440147e817105f62551
Dmitriy Solomahin <solomahin_dima@mail.ru>
2022-12-21 17:27:05 +0300

    add program

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] array = {"hello","2","world",":-)"};
+
+int n = array.Length;
+int L = 0;
+
+for(int i =0;i<n;i++)
+{
+    if(array[i].Length <= 3)
+    {
+        L++;
+    }
+}
+
+string [] newArray = new string[L];
+L = 0;
+
+for(int j =0;j<n;j++)
+{
+    if(array[j].Length <= 3)
+    {
+        newArray[L] = array[j];
+        L++;
+    }
+}
+
+PrintArray(newArray);
+
+
+
+void PrintArray(string[] array){
+    Console.Write("[ ");
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]},");
+    }
+    Console.Write("]");
+}
